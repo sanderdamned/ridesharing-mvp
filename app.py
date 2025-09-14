a76e6837de4190a485d8f36e66f15be9f8744d19
sanderdamned <sander.garnier@live.nl>
2025-09-14 13:24:19 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -81,13 +81,13 @@ def haversine_km(a, b):
 # NHOST AUTH / GRAPHQL
 # ===========================
 def nhost_sign_up(email, password):
-    url = f"{NHOST_AUTH_URL}/signup/email-password"
+    url = f"{NHOST_AUTH_URL}/email-password/signup"
     r = requests.post(url, json={"email": email, "password": password})
     r.raise_for_status()
     return r.json()
 
 def nhost_sign_in(email, password):
-    url = f"{NHOST_AUTH_URL}/signin/email-password"
+    url = f"{NHOST_AUTH_URL}/email-password/signin"
     r = requests.post(url, json={"email": email, "password": password})
     r.raise_for_status()
     return r.json()
